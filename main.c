922cf1b503d7b21a7e495d1dc572f4d55979a102
Bless12ahadjie <blaise)ahadjie@gmail.com>
2023-05-24 14:28:41 +0000

    adding main c

--- main.c
+++ main.c
@@ -3,17 +3,17 @@
  * main - Entry point
  *
  *@environ: takes environment character
- *@Valarg: takes argument vector
+ *@valarg: takes argument vector
  *@argNum: takes argument count
  *
  * Return: Always 0 (success)
  */
 int main(int argNum, char **valarg, char **environ)
 {
-        if (argNum == 1)
-        {
-                output( valarg, environ);
-        }
-        return (0);
+	if (argNum == 1)
+	{
+		output(valarg, environ);
+	}
+	return (0);
 }
 
