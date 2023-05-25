ec49e7da1f8327063c759e791234fa994835efa4
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2023-05-25 17:14:29 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,82 @@
-#include "S_SHell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 /**
- * main - Entry point
+ * output - This function creates a simple shell with
+ * basic commands running
  *
- *@environ: takes environment character
- *@valarg: takes argument vector
- *@argNum: takes argument count
+ * @environ: environment variables
+ *
+ * Return: Always void (success)
+ */
+void output(char **environ)
+{
+
+	char *ptr = NULL;
+	ssize_t buffer;
+	size_t ptrSize = 0;
+	pid_t minPID;
+
+	while (1)
+	{
+		printf("#cisfun$ ");
+
+	buffer = getline(&ptr, &ptrSize, stdin);
+	if (buffer == -1)
+	{
+		free(ptr);
+		exit(EXIT_FAILURE);
+	}
+
+	size_t length = strlen(ptr);
+	if (length > 0 && ptr[length - 1] == '\n')
+		ptr[length - 1] = '\0';
+
+	char *cmd = strtok(ptr, " ");
+	char **args = malloc(sizeof(char *) * (length / 2 + 1));
+	int argCount = 0;
+	while (cmd != NULL)
+	{
+		args[argCount++] = cmd;
+		cmd = strtok(NULL, " ");
+	}
+	args[argCount] = NULL;
+
+	minPID = fork();
+	if (minPID == -1)
+	{
+		free(ptr);
+		free(args);
+		exit(EXIT_FAILURE);
+	}
+	else if (minPID == 0)
+	{
+		if (execve(args[0], args, environ) == -1)
+			printf("%s: No such file or directory\n", args[0]);
+		free(ptr);
+		free(args);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(NULL);
+		free(args);
+	}
+	}
+}
+/**
+ * output - Entry Point
+ *
+ * @environ: environment variables
  *
  * Return: Always 0 (success)
  */
-int main(int argNum, char **valarg, char **environ)
+int main(void)
 {
-	if (argNum == 1)
-	{
-		output(valarg, environ);
-	}
-	return (0);
+	char *environ[] = {NULL}; 
+	output(environ);
+	return 0;
 }
 
